3061b6cad78c5fbd4a7786e1ec25e012d5cf45be
harofour <junhyukseong@gmail.com>
2021-09-15 03:05:52 +0900

    Work on UI

---
+++ app/src/main/java/com/example/mvvm_example/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mvvm_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.mvvm_example.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
